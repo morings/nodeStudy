e634322a10d93cb80aef81c571c18dbee167bbcc
star <1620201780@qq.com>
2020-09-06 10:11:11 +0800

    文档更新

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
 
 const http = require('http')
-const formidable = require('formidable');
-const path = require('path')
+
 http.createServer((req,res)=>{
-  handleFile(req,res,(req,res)=>{
-    console.log(req.body,'\n',req.files);
-    res.end("请求成功")
-  })
+  res.writeHead(200,{
+    'content-type':'text/html;charset=utf-8'
+  });
+  //res.setHeader('content-type', 'text/plain;charset=utf-8')
+  res.end('<div>跳转百度</div>')
 }).listen(3000)
-function hasBody (req) {
-  return 'transfer-encoding' in req.headers || 'content-length' in req.headers;
-};
-function handleData (req, res,handle) {
-  if (hasBody(req)) {
-    var buffers = [];
-    req.on('data', function (chunk) {
-      buffers.push(chunk);
-    });
-    req.on('end', function () {
-      req.rawBody = Buffer.concat(buffers).toString();
-      handle(req, res);
-    });
-  } else {
-    handle(req, res);
-  }
-}
-function handleFile(req, res,handle) {
-  if (hasBody(req)) {
-    if (mime(req) === 'multipart/form-data') {
-      var form = new formidable.IncomingForm();
-      var targetFile = path.join(__dirname,'./upload');
-      form.uploadDir = targetFile;
-      form.parse(req, function(err, fields, files) {
-      req.body = fields;
-      req.files = files;
-      handle(req, res);
-      });
-    }
-  } else {
-    handle(req, res);
-  }
-}
-function mime(req) {
-  var str = req.headers['content-type'] || '';
-  return str.split('; ')[0];
-};
