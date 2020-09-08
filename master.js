e46deb3ff70012f4e669a42ade01374720fec26f
star <star_7799>
2020-09-08 18:08:19 +0800

    cluster

--- master.js
+++ master.js
@@ -4,15 +4,16 @@ cluster.on('listening', (worker, address) => {
   console.log(
     `进程${worker.process.pid}已连接`);
 });
-cluster.on('message',function(msg,handle){
-  if(msg.act=='suicide'){
-    return cluster.fork()
-  }
+cluster.on('exit',function(worker){
+  console.log(
+    `进程${worker.process.pid}已断开`);
+  cluster.fork()
 })
 cluster.setupMaster({
   exec:__dirname+'/worker.js'
 })
 var cpus = os.cpus();
 for(var i=0;i<cpus.length;i++){
-  cluster.fork()
+ cluster.fork()
+ 
 }
--- worker.js
+++ worker.js
@@ -1,12 +1,7 @@
 const http = require('http');
-const cluster = require('cluster');
 const server = http.createServer((req,res)=>{
   res.writeHead(200);
   res.end(process.pid+'：Hello World')
 }).listen(8000)
-var worker = cluster.worker;
-console.log(process.pid)
-process.on('uncaughtException',function(){
-  console.log(12)
-})
+
 
